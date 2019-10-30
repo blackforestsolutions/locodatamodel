7ec8871aa3259a3f554864f3e4090b6bff229d6c
hennihaus <jan-hendrik.hausner@outlook.com>
2019-10-30 22:08:28 +0100

    I added the stationId to the datamodel.

--- src/main/java/de/blackforestsolutions/datamodel/ApiTokenAndUrlInformation.java
+++ src/main/java/de/blackforestsolutions/datamodel/ApiTokenAndUrlInformation.java
@@ -279,6 +279,9 @@ public class ApiTokenAndUrlInformation {
         private String authorization;
         @Getter
         @Setter
+        private String stationId;
+        @Getter
+        @Setter
         private String germanRailJourneyDeatilsPath;
         @Getter
         @Setter
