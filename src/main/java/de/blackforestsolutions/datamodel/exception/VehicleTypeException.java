213d9ff29e6d60b468ee1aa0b493cd35b196f627
hennihaus <jan-hendrik.hausner@outlook.com>
2020-04-24 19:56:47 +0200

    made test runnable again.

--- src/main/java/de/blackforestsolutions/datamodel/exception/VehicleTypeException.java
+++
@@ -1,8 +0,0 @@
-package de.blackforestsolutions.datamodel.exception;
-
-public class VehicleTypeException extends Exception {
-
-    public VehicleTypeException(Class c) {
-        super("No vehicle type found in: ".concat(c.getName()));
-    }
-}
