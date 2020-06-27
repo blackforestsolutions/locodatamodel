a8bf5299e4034cb2cc4116553198be08c375be5d
hennihaus <jan-hendrik.hausner@outlook.com>
2020-06-27 09:12:40 +0200

    we ignore null fields in jsonMapper and added travelpointstatus

---
+++ src/main/java/de/blackforestsolutions/datamodel/TravelPointStatus.java
@@ -0,0 +1,9 @@
+package de.blackforestsolutions.datamodel;
+
+import java.util.Optional;
+
+public class TravelPointStatus {
+
+    private Optional<TravelPoint> travelPoint;
+    private Optional<Problem> problem;
+}
--- src/main/java/de/blackforestsolutions/datamodel/util/LocoJsonMapper.java
+++ src/main/java/de/blackforestsolutions/datamodel/util/LocoJsonMapper.java
@@ -1,5 +1,6 @@
 package de.blackforestsolutions.datamodel.util;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -15,6 +16,7 @@ public class LocoJsonMapper {
 
     public LocoJsonMapper() {
         this.mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         mapper.registerModule(new JavaTimeModule());
         mapper.registerModule(new GeoModule());
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
