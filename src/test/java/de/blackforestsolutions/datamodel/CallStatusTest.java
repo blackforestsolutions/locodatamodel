18813924309a0b467da0ec1adb44894fda42ac20
hennihaus <jan-hendrik.hausner@outlook.com>
2020-08-19 21:47:56 +0200

    made build runnable

--- src/test/java/de/blackforestsolutions/datamodel/CallStatusTest.java
+++ src/test/java/de/blackforestsolutions/datamodel/CallStatusTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static de.blackforestsolutions.datamodel.util.objectmothers.CallStatusObjectMother.getCallStatusTravelPoint;
 import static de.blackforestsolutions.datamodel.util.objectmothers.CallStatusObjectMother.getJsonCallStatusTravelPoint;
-import static org.apache.commons.lang3.StringUtils.deleteWhitespace;
+import static org.apache.commons.lang.StringUtils.deleteWhitespace;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
